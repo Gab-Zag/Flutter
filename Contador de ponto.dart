05d11bdb2ea6794b86d7be2d0057c4ab5d4bdee0
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2024-02-08 08:59:16 -0300

    Update Contador de ponto.dart

--- Contador de ponto.dart
+++ Contador de ponto.dart
@@ -1,6 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 void main() {
   runApp(const MyApp());
+}
+
+mixin _portraitModeMixin on StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    _portraitModeOnly();
+    return super.build(context);
+  }
+}
+
+mixin PortraitStatefulModeMixin<T extends StatefulWidget> on State<T>{
+  @override
+  Widget build(BuildContext context){
+    _portraitModeOnly();
+    return super.build(context);
+  }
+  @override
+  void dispose(){
+    _enableRotation();
+    super.dispose();
+  }
+}
+
+void _portraitModeOnly(){
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+}
+
+void _enableRotation(){
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
 }
 
 class MyApp extends StatelessWidget {
